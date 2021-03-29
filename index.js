55a44fc8d2f3b225707bcdbec2c5cf98e4dc6422
b00kbaker <caimmurphy@gmail.com>
2021-03-28 21:33:11 -0600

    Switch case

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const connection = mysql.createConnection({
     database: 'employ_DB',
   });
   
-//   connection.connect((err) => {
-//     if (err) throw err;
-//     ();
-//   });
+  connection.connect((err) => {
+    if (err) throw err;
+    startQA();
+});
   
   
 
@@ -24,8 +24,69 @@ const connection = mysql.createConnection({
         // -What role from (x) department? (input choice)
         // -What salary amount? (input type)
         // -Who will be their manager? (input choice)
-    // -Would you like to remove an employee?
+    // -Would you like to remove an employee? (list of full name)
     // -Would you like to update an employee's role?
+        //  The employee is in which department?
+        // All names in (x) department?
     // -Would you like to update an employee's overviewing manager?
+        // -The employee is in which department?
+        // All names in (x) department
+        // Choose employee then select manger
 //  -View all employees by departments? List the 4 departments
 //  -View all employees by roles? All roles from all departments
+
+const startQA = () => {
+    inquirer.prompt({
+        name: 'begin',
+        type: 'choices',
+        message: 'What would you like to do?',
+        choices: [
+          'View all employees',
+          'View employees by department',
+          'View employees by roles',
+          'Add a new employee',
+          'Remove an employee',
+          'Update an employee role',
+          'Choose an employee to move under a new manager',
+        ],
+      })
+      .then((answer) => {
+        switch (answer.action) {
+          case 'View all employees':
+            allSearch();
+            break;
+        }
+        switch (answer.action) {
+            case 'View employees by department':
+              employeeDepartment();
+              break;
+        }
+        switch (answer.action) {
+            case 'View employees by roles':
+              employeeRoles();
+              break;
+        }
+        switch (answer.action) {
+            case 'Add a new employee':
+              addEmployee();
+              break;
+        }
+        switch (answer.action) {
+            case 'Remove an employee':
+              removeEmployee();
+              break;
+        }
+        switch (answer.action) {
+            case 'Update an employee role':
+              updateRole();
+              break;
+        }
+        switch (answer.action) {
+            case 'Choose an employee to move under a new manager':
+              newManager();
+              break;
+        }
+    })
+};
+
+
